5f6fa8e69cc863273d65dfe57f34c6fa3ab0905f
Lakha Singh <polizic@yahoo.co.in>
2015-04-19 12:40:20 +0530

    removed requirejs config and moved to require-config.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,6 @@
 /*global require*/
 'use strict';
 
-require.config({
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
-    },
-    paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/lodash/dist/lodash',
-        bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap',
-        vSearch: 'views/search',
-        rMain: 'routes/main'
-    }
-});
-
 require([
     'backbone',
     'rMain'
