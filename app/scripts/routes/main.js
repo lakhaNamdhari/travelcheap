29958783e84430c5609e49c261cc3101446a6b53
Lakha Singh <polizic@yahoo.co.in>
2015-04-20 14:43:20 +0530

    added routeTo helper, to be used by views to switch pages. Added route for search-results page. Added destroy view funct for current two routes

--- app/scripts/routes/main.js
+++ app/scripts/routes/main.js
@@ -3,25 +3,70 @@
 define([
     'jquery',
     'backbone',
-    'vSearch'
-], function ($, Backbone, SearchView) {
+    'vSearch',
+    'vTrips',
+    'cTrip'
+], function ($, Backbone, SearchView, TripsView, TripCollection) {
     'use strict';
 
     var MainRouter = Backbone.Router.extend({
         routes: {
-        	"" : "rSearch"
+        	'' : 'rSearch',
+            '/search-results/:source/:destination' : 'rSearchResults'
         },
 
-        $viewHolder: $("#view"),
+        $viewHolder: $('#view'),
 
         /*
         *   Seach page, which is default route.
         */
         rSearch: function(){
-        	this.currentView = new SearchView({
+            // Destroy any Existing View
+            if ( this.currentView ){
+                this.currentView.destroy();
+            }
+
+            // Load required View
+            this.currentView = new SearchView({
                 model: {},
                 $parent: this.$viewHolder
             });
+        },
+
+        /*
+        *   Seach Results page
+        */
+        rSearchResults: function( source, destination ){
+            var collection;
+
+            // Destroy any Existing View
+            if ( this.currentView ){
+                this.currentView.destroy();
+            }
+
+            collection = new TripCollection({
+                source: source,
+                destination: destination
+            });
+
+            // Load required View
+        	this.currentView = new SearchResultsView({
+                model: {},
+                $parent: this.$viewHolder
+            });
+        },
+
+        /*
+        *   Utility for routing to given page
+        */
+        routeTo: function( page , args ){
+            if ( typeof page === 'string' && page.length ){
+                page = '/' + page;
+                if ( args && args.length ){
+                    page = page + '/' + args.join('/');
+                }
+                history.pushState( page );
+            }
         }
     });
 
